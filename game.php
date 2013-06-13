641ec77c38364f75da423ba35e7d60eef30a36ef
maxgal <maxrusgal@gmail.com>
2013-06-13 13:55:48 +0800

    Update game.php

--- game.php
+++ game.php
@@ -2,4 +2,8 @@
 
 session_start(); //launch sessions
 
-check_login(); //Проверяем не пришло ли данных с формы авторизации
+if (!check_login()) //Проверяем не пришло ли данных с формы авторизации
+{
+  show_login_page();
+  show_reg_page();
+}
